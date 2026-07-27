6b0afb18975295f69e0f27c8d6819277b41092ec
bb7073 <bb7073@users.noreply.github.com>
2026-07-27 04:40:25 +0000

    말풍선 가독성, 캐시 v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'gonmae-v3';
+const C = 'gonmae-v4';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
