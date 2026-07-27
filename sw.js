a6d09bc8624ee1b20da31db50e7de7b42de9982c
bb7073 <bb7073@users.noreply.github.com>
2026-07-27 03:49:00 +0000

    좌표 보강 재판정, 기타지구 태그 제외

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'gonmae-v2';
+const C = 'gonmae-v3';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
