addec29afd437a666dead874c4c9ad5cacf18b3e
bb7073 <bb7073@users.noreply.github.com>
2026-07-27 16:00:14 +0000

    zoneonly filter, 관리형 ex

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'gonmae-v9';
+const C = 'gonmae-v10';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
