b06186bb6540b07b4dca71126d4f29973a0631a4
bb7073 <bb7073@users.noreply.github.com>
2026-08-02 04:15:07 +0000

    isNew per-source date, sw v12

--- sw.js
+++ sw.js
@@ -1,14 +1,8 @@
-const C = 'gonmae-v11';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).then(r => {
-      const c = r.clone();
-      caches.open(C).then(cache => cache.put(e.request, c));
-      return r;
-    }).catch(() => caches.match(e.request))
-  );
+const C="gonmae-v12";
+self.addEventListener("install",e=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>n!==C&&caches.delete(n)))).then(()=>clients.claim())));
+self.addEventListener("fetch",e=>{
+if(e.request.method!=="GET")return;
+if(e.request.url.indexOf("/photos/")>-1)return;
+e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));
 });
-
