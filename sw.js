41a4d4d8ac892a349890a2842a8d747b7caa81ed
bb7073 <bb7073@users.noreply.github.com>
2026-08-05 06:07:10 +0000

    isNew by KST date, sw v13 navigate reload

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const C="gonmae-v12";
+const C="gonmae-v13";
 self.addEventListener("install",e=>self.skipWaiting());
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>n!==C&&caches.delete(n)))).then(()=>clients.claim())));
 self.addEventListener("fetch",e=>{
 if(e.request.method!=="GET")return;
 if(e.request.url.indexOf("/photos/")>-1)return;
+if(e.request.mode==="navigate"){e.respondWith(fetch(e.request,{cache:"reload"}).catch(()=>caches.match(e.request)));return;}
 e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));
 });
